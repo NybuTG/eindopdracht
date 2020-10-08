d7716af47937ce5f171c8f89944f2aa74208747e
NybuTG <derk.jan.weenign@gmail.com>
2020-10-08 13:49:28 +0200

    Push MASSIVE backlog ...lol whoops

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-// kourbatov.com/faq/operatin.htm - To get the users operating system
+// Sources:
+// - hhtps://kourbatov.com/faq/operatin.htm - To get the concept down
+// - https://medium.com/@justenphelps/mobile-os-detection-1f14b46565cc - Works better
 
 function getOs() {
 	var osName = "unkown"
 	
-	if (navigator.appVersion.indexOf("Win") !=- 1) osName="windows";
-	if (navigator.appVersion.indexOf("Mac") !=- 1) osName="mac";
-	if (navigator.appVersion.indexOf("X11") !=- 1) osName="unix / linux";
-	if (navigator.appVersion.indexOf("Linux") !=- 1) osName="linux";
+	console.log(navigator.platform)
+	if (navigator.platform.indexOf("Win") !=- 1) osName="windows";
+	if (navigator.platform.indexOf("Mac") !=- 1) osName="macos";
+	if (navigator.platform.indexOf("Mac") !=- 1) osName="unix";
+	if (navigator.platform.indexOf("Mac") !=- 1) osName="ios";
+	if (navigator.platform.indexOf("Mac") !=- 1) osName="android";
+	if (navigator.platform.indexOf("Linux") !=- 1) osName="linux";
 	
-	console.log(osName)
 	document.write(osName);
 }
